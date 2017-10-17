0ff37bfaf06ed181f308d3770b759fd1d3f77576
ankit01ojha <anoj123anru@gmail.com>
2017-10-18 01:16:54 +0530

    added a view and added comments and other changes

--- tournament.sql
+++ tournament.sql
@@ -1,22 +1,36 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+-- Dropping the database if exists , for a fresh start everytime the file is imported.
+DROP DATABASE IF EXISTS tournament ;
 
+--creating the database tournament.
+CREATE DATABASE tournament ;
+
+--Connecting to the database tournament.
+\c tournament
+
+-- Dropping all the tables , for a fresh start everytime the file is imported.
+DROP TABLE IF EXISTS information ;
+DROP TABLE IF EXISTS matches ;
+
+--Dropping views if they exists.
+DROP VIEW IF EXISTS score ;
+
+--This table is for the player's information.
 CREATE TABLE information (
-            id serial,
+            id serial PRIMARY KEY,
             name text
 ) ;
 
-CREATE TABLE score_table (
-            id serial,
-            name text,
-            wins integer,
-            matches integer,
-            score integer
-
+-- This table is for keeping the records of the matches played by a player.
+CREATE TABLE matches (
+            match_id integer PRIMARY KEY ,
+            winner integer,
+            loser integer
 ) ;
 
+-- This creates a view which counts the wins of a player and the number of matches played by that player in the tournament.
+CREATE view score AS
+select player.id, player.name,
+(select count(*) from matches where player.id=winner) as wins,
+(select count(*) from matches where player.id in (winner,loser)) as matches_played
+from player
+group by player.id order by wins desc ;
